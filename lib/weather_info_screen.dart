222a5c81fc8bda3241317090bb754197eb38e273
Kota1021 <ko_matsumoto@yumemi.co.jp>
2024-09-10 20:59:41 +0900

    impl

---
+++ lib/weather_info_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class WeatherInfoScreen extends StatelessWidget {
+  const WeatherInfoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final labelLargeStyle = Theme.of(context).textTheme.labelLarge;
+    return Center(
+      child: FractionallySizedBox(
+        widthFactor: 0.5,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Spacer(),
+            Column(children: [
+              const AspectRatio(
+                aspectRatio: 1,
+                child: Placeholder(),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16, bottom: 16),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        '**℃',
+                        textAlign: TextAlign.center,
+                        style: labelLargeStyle?.copyWith(color: Colors.blue),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        '**℃',
+                        textAlign: TextAlign.center,
+                        style: labelLargeStyle?.copyWith(color: Colors.red),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Close',
+                        textAlign: TextAlign.center,
+                        style: labelLargeStyle?.copyWith(color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Reload',
+                        textAlign: TextAlign.center,
+                        style: labelLargeStyle?.copyWith(color: Colors.blue),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
