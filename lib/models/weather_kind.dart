5263e06ecbc5dc965f8a5120848e3c7049db7b70
Kota1021 <ko_matsumoto@yumemi.co.jp>
2024-10-03 20:54:43 +0900

    add semanticsLabel to each icon

--- lib/models/weather_kind.dart
+++ lib/models/weather_kind.dart
@@ -1,11 +1,28 @@
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 enum WeatherKind { sunny, cloudy, rainy }
 
 extension WeatherKindIcon on WeatherKind {
+  @visibleForTesting
+  static const sunnyLabel = 'Sunny image';
+  @visibleForTesting
+  static const cloudyLabel = 'Cloudy image';
+  @visibleForTesting
+  static const rainyLabel = 'Rainy image';
+
   SvgPicture get svgImage => switch (this) {
-        WeatherKind.sunny => SvgPicture.asset('images/Sunny.svg'),
-        WeatherKind.cloudy => SvgPicture.asset('images/Cloudy.svg'),
-        WeatherKind.rainy => SvgPicture.asset('images/Rainy.svg'),
+        WeatherKind.sunny => SvgPicture.asset(
+            'images/Sunny.svg',
+            semanticsLabel: sunnyLabel,
+          ),
+        WeatherKind.cloudy => SvgPicture.asset(
+            'images/Cloudy.svg',
+            semanticsLabel: cloudyLabel,
+          ),
+        WeatherKind.rainy => SvgPicture.asset(
+            'images/Rainy.svg',
+            semanticsLabel: rainyLabel,
+          ),
       };
 }
